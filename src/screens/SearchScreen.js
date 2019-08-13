fee43dab20d4374539bf4eea1f1f533d8267b60b
Ben-Mustion <benmustion@gmail.com>
2019-08-13 10:30:01 -0700

    yelp api setup

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -4,11 +4,14 @@ import SearchBar from '../components/SearchBar';
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
+  
   return (
     <View>
       <SearchBar 
         term={term} 
-        onTermChange={newTerm => setTerm(newTerm)} />
+        onTermChange={newTerm => setTerm(newTerm)} 
+        onTermSubmit={() => console.log('term was submitted')}
+      />
       <Text>Search Screen</Text>
       <Text>{term}</Text>
     </View>
